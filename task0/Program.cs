18705364b6e13e3fba90cd335c00d0ea00df8e5e
Alexjur5676 <alex9992142@mail.ru>
2022-08-05 01:43:17 +0700

    Add tasks

---
+++ task0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход 
+//принимает число и выдает его 
+//квадрат (число умноженное на само себя)
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number * number);
+
---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB * numberB == numberA)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
