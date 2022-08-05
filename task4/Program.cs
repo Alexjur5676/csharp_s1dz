1fb923158200d0ec27ef89138a6067bb2707c12b
Alexjur5676 <alex9992142@mail.ru>
2022-08-05 11:06:17 +0700

    Add task6

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,9 +2,9 @@
 // числа и выдаёт максимальное из этих чисел.
 Console.WriteLine("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите третье число: ");
 int numberC = Convert.ToInt32(Console.ReadLine());
 int max = numberA;
 
